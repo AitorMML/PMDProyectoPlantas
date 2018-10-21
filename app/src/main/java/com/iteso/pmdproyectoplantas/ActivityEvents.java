f0055f166877306efdd64e4c857c60efec575170
AitorMML <is702449@iteso.mx>
2018-10-20 19:27:52 -0500

    Login mock 1

---
+++ app/src/main/java/com/iteso/pmdproyectoplantas/ActivityEvents.java
@@ -0,0 +1,13 @@
+package com.iteso.pmdproyectoplantas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_events);
+    }
+}
